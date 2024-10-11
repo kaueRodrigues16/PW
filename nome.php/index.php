b19740c3259f603c07955d19d0bd2b60a2f9a6ae
kaueRodrigues16 <142915945+kaueRodrigues16@users.noreply.github.com>
2024-10-10 21:15:39 -0300

    Add files via upload

---
+++ nome.php/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>meu primeeiro site php</title>
+</head>
+<body>
+    <header class="header"> 
+     <nav> 
+
+     <ul class="nav">
+      <li><a href="https://www.youtube.com/watch?v=Z4CbaGCEsTY" target="_blank">link</a></li>
+   
+      <li><a href="pag2.php">pagina2</a></li>
+    
+    </ul>
+    <form class="f">
+      <label for="pesquisa">pesquisa</label>
+        <input type="text" id="pesquisa" placeholder="pesquisa">
+        <input type="submit" value="enviar">
+    </form>
+     </nav>
+<!--quando precisar atualisar aperte ctrl+f5-->
+    </header>
+
+    <br>
+<diV class="formulario">
+<form >
+<div class="f2">
+    <ul>
+    <li ><input type="text"></li>
+    <li><input type="submit" value="enviar"></li>
+    <li ><input type="text"></li>
+    <li><input type="text"></li>
+</div>
+</ul>
+</form>
+</div>
+    
+</body>
+</html>
---
+++ nome.php/pag2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    
+<header class="header"> 
+     <nav> 
+
+     <ul class="nav">
+      <li><a href="https://www.youtube.com/watch?v=Z4CbaGCEsTY" target="_blank">link</a></li>
+      <li><a href="https://www.youtube.com/watch?v=Z4CbaGCEsTY" target="_blank">link</a></li>
+      <li><a href="index.php">pagina1</a></li>
+    
+    </ul>
+    <form class="f">
+      <label for="pesquisa">pesquisa</label>
+        <input type="text" id="pesquisa" placeholder="pesquisa">
+    </form>
+     </nav>
+<!--quando precisar atualisar aperte ctrl+f5-->
+    
+   
+</body>
+</html>
