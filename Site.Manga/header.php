ef2ed2272d720ce831a25bed47261671df9ad68c
kaueRodrigues16 <142915945+kaueRodrigues16@users.noreply.github.com>
2025-03-05 23:37:56 -0300

    Add files via upload

---
+++ Site.Manga/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<header class="container">
+    <a class="link" href="https://www.youtube.com/watch?v=yZrL5QMxeOY&list=PLqdxJj_z7crBiLUDHE7AMxgU9a7Gib21Y">manga<a>
+    <a class="link" href="https://www.youtube.com/watch?v=yZrL5QMxeOY&list=PLqdxJj_z7crBiLUDHE7AMxgU9a7Gib21Y">manga<a>
+    <a class="link" href="https://www.youtube.com/watch?v=yZrL5QMxeOY&list=PLqdxJj_z7crBiLUDHE7AMxgU9a7Gib21Y">manga<a>
+</header>
+</body>
+</html>
